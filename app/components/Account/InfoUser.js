0525e770484f14698d924c6e665a4a0b9f4be8fe
kevin <kevinalvapearson777@gmail.com>
2021-02-20 18:05:07 -0500

    subiendo el avatar al storage

--- app/components/Account/InfoUser.js
+++ app/components/Account/InfoUser.js
@@ -1,18 +1,53 @@
 import React from "react";
 import { StyleSheet, View, Text } from 'react-native';
 import { Avatar } from "react-native-elements";
+import * as firebase from "firebase";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
 
 export default function InfoUser(props) {
-    const { userInfo: {photoURL, displayName, email} } = props;
-    console.log(photoURL);
-    console.log(displayName);
-    console.log(email);
+    const { userInfo: { uid, photoURL, displayName, email}, toastRef } = props;
+    
+    const changeAvatar = async () => {
+        const resultPermision = await Permissions.askAsync(Permissions.CAMERA);
+        const resultPermissionCamera = resultPermision.permissions.camera.status;
+
+        if (resultPermissionCamera === "denied") {
+            toastRef.current.show("Es necesario aceptar los permisos de la galería")
+        } else {
+            const result = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true, aspect: [4,3]
+            });
+            
+            if (result.cancelled) {
+                toastRef.current.show("Has cerrado la seleccion de imagenes");
+            } else {
+                uploadImage(result.uri)
+                .then(() => {
+                    console.log("Imagen subida");
+                })
+                .catch(() =>{
+                    toastRef.current.show("Error al actualizar el avatar");
+                })
+            }
+        }
+    };
+
+    const uploadImage = async (uri) => {
+        const response = await fetch(uri);
+        const blob = await response.blob();
+
+        const ref = firebase.storage().ref().child(`avatar/${uid}`);
+        return ref.put(blob);
+    }
+
     return(
         <View style={styles.viewUserInfo}>
             <Avatar 
                 rounded
                 size="large"
                 showEditButton
+                onEditPress={changeAvatar}
                 containerStyle={styles.userInfoAvatar}
                 source={photoURL ? { uri: photoURL}: require("../../../assets/img/avatar-default.jpg")}
             />

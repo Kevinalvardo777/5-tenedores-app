ce05a82be5f2d425267f5bd011d63f8e10d1b9e2
kevin <kevinalvapearson777@gmail.com>
2021-02-20 19:00:50 -0500

    Completando el menu de ListItem

--- app/components/Account/AccountOptions.js
+++ app/components/Account/AccountOptions.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { StyleSheet, View, Text} from "react-native";
+import { StyleSheet, View } from "react-native";
 import { ListItem} from "react-native-elements";
 import { map } from "lodash";
 
 export default function AccountOptions(props) {
     const { userInfo, toastRef } = props;
-    const menuOptions = generateOptions();
-    console.log(menuOptions);
+    //console.log(menuOptions);
+
+    const selectComponent = (key) => {
+        console.log("Click!!!")
+        console.log(key);
+    }
+
+    const menuOptions = generateOptions(selectComponent);
+
 
     return (
         <View>
@@ -14,26 +21,59 @@ export default function AccountOptions(props) {
                 <ListItem 
                     key={index}
                     title={menu.title}
+                    leftIcon={{
+                        type: menu.iconType, 
+                        name: menu.iconNameLeft, 
+                        color: menu.iconColorLeft, 
+                    }}
+                    rightIcon={{
+                        type: menu.iconType, 
+                        name: menu.iconNameRight, 
+                        color: menu.iconColorRight
+                    }}
+                    containerStyle={styles.menuItem}
+                    onPress={menu.onPress}
                 />
             ))}
         </View>
     );
 }
 
-function generateOptions() {
+function generateOptions(selectComponent) {
     return [
         {
             title: "Cambiar nombre y apellidos", 
+            iconType: "material-community", 
+            iconNameLeft: "account-circle", 
+            iconColorLeft: "#ccc",
+            iconNameRight: "chevron-right",
+            iconColorRight: "#ccc",
+            onPress: () => selectComponent("displayName")
         }, 
         {
-            title: "Cambiar email"
+            title: "Cambiar email", 
+            iconType: "material-community", 
+            iconNameLeft: "at", 
+            iconColorLeft: "#ccc", 
+            iconNameRight: "chevron-right",
+            iconColorRight: "#ccc", 
+            onPress: () => selectComponent("email")
         }, 
         {
-            title: "Cambiar contraseña"
+            title: "Cambiar contraseña", 
+            iconType: "material-community", 
+            iconNameLeft: "lock-reset", 
+            iconColorLeft: "#ccc",
+            iconNameRight: "chevron-right",
+            iconColorRight: "#ccc", 
+            onPress: () => selectComponent("password")
         }
     ]
 }
 
 const styles = StyleSheet.create({
-
+    menuItem: {
+        borderBottomWidth: 1, 
+        borderBottomColor: "#e3e3e3"
+    }
 });

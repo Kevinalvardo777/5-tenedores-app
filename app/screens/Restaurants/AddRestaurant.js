68087ee74a65983d129b705a20c849b0cb860332
kevin <kevinalvapearson777@gmail.com>
2021-02-22 14:40:47 -0500

    formulario de alta de restaurante

--- app/screens/Restaurants/AddRestaurant.js
+++ app/screens/Restaurants/AddRestaurant.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {useState, useRef } from "react";
 import { StyleSheet, View, Text } from "react-native";
+import Toast from "react-native-easy-toast";
+import Loading from "../../components/Loading";
+import AddRestaurantForm from "../../components/Restaurants/AddRestaurantForm";
 
-export default function AddRestaurant() {
-    return (
+export default function AddRestaurant(props) {
+    const { navigation } = props;
+    const [isLoading, setIsLoading] = useState(false);
+    const toastRef = useRef();
+    return ( 
         <View>
-            <Text>AddRestaurant...</Text>
+            <AddRestaurantForm 
+                toastRef={toastRef} 
+                setIsLoading={setIsLoading}
+                navigation={navigation}
+            />
+            <Toast ref={toastRef} postion="center" opacity={0.9} />
+            <Loading isVisible={isLoading} text="Creando restaurante" />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    
+
 })

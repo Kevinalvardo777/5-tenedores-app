13e501050dae5d99e7579f599dc8052a8b1f85ef
kevin <kevinalvapearson777@gmail.com>
2021-03-14 15:00:54 -0500

    titulo y puntuacion del restaurante

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { Rating } from "react-native-elements";
 import Loading from "../../components/Loading";
 import Carousel from "../../components/Carousel";
 
@@ -14,6 +15,7 @@ export default function Restaurant(props) {
     const { navigation, route} = props;
     const { id, name } = route.params;
     const [restaurant, setRestaurant] = useState(null); 
+    const [rating, setRating] = useState(0);
 
     navigation.setOptions({ title: name });
 
@@ -25,6 +27,7 @@ export default function Restaurant(props) {
                 const data = response.data();
                 data.id = response.id;
                 setRestaurant(data);
+                setRaiting(data.rating);
             });
     }, []);
 
@@ -37,7 +40,31 @@ export default function Restaurant(props) {
                 height={250}
                 width={screenWidth}
             />
+            <TitleRestaurant 
+                name={restaurant.name}
+                description={restaurant.description}
+                rating={restaurant.rating}
+            />
         </ScrollView>
+    )
+}
+
+function TitleRestaurant(props) {
+    const { name, description, rating } = props;
+
+    return (
+        <View style={styles.viewRestaurantTitle}>
+            <View style={{ flexDirection: "row"}}>
+                <Text style={styles.nameRestaurant}>{name}</Text>
+                <Rating 
+                    style={styles.rating}
+                    imageSize={20}
+                    readonly
+                    startingValue={parseFloat(rating)}
+                />
+            </View>
+            <Text style={styles.descriptionRestaurant}>{description}</Text>
+        </View>
     )
 }
 
@@ -45,5 +72,20 @@ const styles = StyleSheet.create({
     viewBody: {
         flex: 1, 
         backgroundColor: "#fff"
+    }, 
+    viewRestaurantTitle: {
+        padding: 15
+    }, 
+    nameRestaurant: {
+        fontSize: 20,
+        fontWeight: "bold"
+    }, 
+    descriptionRestaurant: {
+        marginTop: 5,
+        color: "grey"
+    }, 
+    rating: {
+        position: "absolute",
+        right: 0
     }
 })

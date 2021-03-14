5e8d1073032f329baa2d5a5a9ffd935546ab4ef1
kevin <kevinalvapearson777@gmail.com>
2021-03-14 14:22:01 -0500

    loading mientras se carga la informacion del restaurante

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Loading from "../../components/Loading";
+
+import { firebaseApp } from "../../utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
 
 export default function Restaurant(props) {
     const { navigation, route} = props;
     const { id, name } = route.params;
-    
+    const [restaurant, setRestaurant] = useState(null);
+
     navigation.setOptions({ title: name });
+
+    useEffect(() => {
+        db.collection("restaurants")
+            .doc(id)
+            .get()
+            .then((response) => {
+                const data = response.data();
+                data.id = response.id;
+                setRestaurant(data);
+            });
+    }, []);
+
+    if(!restaurant) return <Loading isVisible={true} text="Cargando..." />
 
     return (
         <View>

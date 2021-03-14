dbbb576424ef82f0bee4bc2b7f75e5591982aac3
kevin <kevinalvapearson777@gmail.com>
2021-03-14 14:44:01 -0500

    añadiendo un carousel con las imagenes del restaurante

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
 import Loading from "../../components/Loading";
+import Carousel from "../../components/Carousel";
 
 import { firebaseApp } from "../../utils/firebase";
 import firebase from "firebase/app";
 import "firebase/firestore";
 
 const db = firebase.firestore(firebaseApp);
+const screenWidth = Dimensions.get("window").width;
 
 export default function Restaurant(props) {
     const { navigation, route} = props;
     const { id, name } = route.params;
-    const [restaurant, setRestaurant] = useState(null);
+    const [restaurant, setRestaurant] = useState(null); 
 
     navigation.setOptions({ title: name });
 
@@ -29,10 +31,19 @@ export default function Restaurant(props) {
     if(!restaurant) return <Loading isVisible={true} text="Cargando..." />
 
     return (
-        <View>
-            <Text>Restaurante info</Text>
-        </View>
+        <ScrollView vertical style={styles.viewBody}>
+            <Carousel
+                arrayImages={restaurant.images}
+                height={250}
+                width={screenWidth}
+            />
+        </ScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1, 
+        backgroundColor: "#fff"
+    }
+})

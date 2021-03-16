dd5d0771dd25ad96d6ae611101ea2a4e7de4386e
kevin <kevinalvapearson777@gmail.com>
2021-03-16 14:11:18 -0500

    formulario para crear comentarios

--- app/screens/Restaurants/AddReviewRestaurant.js
+++ app/screens/Restaurants/AddReviewRestaurant.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { AirbnbRating, Button, Input } from "react-native-elements";
 
 export default function AddReviewRestaurant(props) {
     const { navigation, route } = props;
@@ -8,10 +9,66 @@ export default function AddReviewRestaurant(props) {
     console.log(idRestaurant);
 
     return (
-        <View>
-            <Text>AddReviewRestaurant</Text>
+        <View style={styles.viewBody}>
+            <View style={styles.viewRating}>
+                <AirbnbRating 
+                    count={5}
+                    reviews={["Pésimo", "Deficiente", "Normal", "Muy Bueno", "Excelent"]}
+                    defaultRating={0}
+                    size={35}
+                />
+            </View>
+            <View style={styles.formReview}>
+                <Input 
+                    placeholder="Titulo"
+                    containerStyle={styles.input}
+                />
+                <Input 
+                    title="Comentario..."
+                    multiline={true}
+                    inputContainerStyle={styles.textArea}
+                />
+                <Button 
+                    title="Enviar comentario"
+                    containerStyle={styles.btnContainer}
+                    buttonStyle={styles.btn}
+                />
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1,
+    }, 
+    viewRating: {
+        height: 110,
+        backgroundColor: "#f2f2f2"
+    }, 
+    formReview: {
+        flex: 1, 
+        alignItems: "center",
+        margin: 10,
+        marginTop: 40
+    }, 
+    input: {
+        marginBottom: 10
+    }, 
+    textArea: {
+        height: 150, 
+        width: "100%", 
+        padding: 0, 
+        margin: 0
+    }, 
+    btnContainer: {
+        flex: 1, 
+        justifyContent: "flex-end",
+        marginTop: 20,
+        marginBottom: 10,
+        width: "95%"
+    }, 
+    btn: {
+        backgroundColor: "#00a680"
+    }
+})

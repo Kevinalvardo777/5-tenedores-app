c34d55d4b6e9bb350325977a1312143998bcbae4
kevin <kevinalvapearson777@gmail.com>
2021-03-16 15:35:25 -0500

    Listado de comentarios

--- app/components/Restaurants/ListReviews.js
+++ app/components/Restaurants/ListReviews.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, Avatar, Rating } from "react-native-elements";
-
+import { map } from "lodash";
 import { firebaseApp } from "../../utils/firebase";
 import firebase from "firebase/app";
 import "firebase/firestore";
@@ -65,6 +65,42 @@ export default function ListReviews(props) {
                     </Text>
                 </View>
             )}
+             {map(reviews, (review, index) => (
+                    <Review 
+                        key={index} review={review}
+                    />
+                ))}
+        </View>
+    )
+}
+
+function Review(props){
+    const { title, review, rating, createAt, avatarUser } = props.review;
+    const createReview = new Date(createAt.seconds * 1000);
+
+    return (
+        <View style={styles.viewReview}>
+            <View style={styles.viewImageAvatar}>
+                <Avatar 
+                    size="large"
+                    rounded
+                    containerStyle={styles.imageAvatarUser}
+                    source={avatarUser ? { uri: avatarUser} : require("../../../assets/img/avatar-default.jpg")}
+                />
+            </View>
+            <View style={styles.viewInfo}>
+                <Text style={styles.reviewTitle}>{title}</Text>
+                <Text style={styles.reviewText}>{review}</Text>
+                <Rating 
+                    imageSize={15}
+                    startingValue={rating}
+                    readonly
+                />
+                <Text style={styles.reviewData}>
+                    {createReview.getDate()}/{createReview.getMonth() + 1}/{createReview.getFullYear()} - 
+                    {createReview.getHours()}:{createReview.getMinutes() < 10 ? "0" : ""}{createReview.getMinutes()}
+                    </Text>
+            </View>
         </View>
     )
 }
@@ -75,5 +111,37 @@ const styles = StyleSheet.create({
     }, 
     btnTitleReview: {
         color: "#00a680"
+    }, 
+    viewReview: {
+        flexDirection: "row",
+        padding: 10,
+        paddingBottom: 20,
+        borderBottomColor: "#e3e3e3",
+        borderBottomWidth: 1
+    },
+    imageAvatarUser: {
+        width: 50,
+        height:50
+    }, 
+    viewInfo: {
+        flex: 1, 
+        alignItems: "flex-start",
+        marginLeft: 10,
+    }, 
+    reviewTitle: {
+        fontWeight: "bold"
+    }, 
+    reviewText: {
+        paddingTop: 2, 
+        color: "grey",
+        marginBottom: 5
+    }, 
+    reviewData: {
+        marginTop: 5, 
+        color: "grey",
+        fontSize: 12, 
+        position: "absolute",
+        right: 0, 
+        bottom: 0
     }
 })
